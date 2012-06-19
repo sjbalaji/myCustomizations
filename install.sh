117b35b0ce0f28f72ff99fed3679c2710227a64f
balaji.sj <sjbalajimdu@gmail.com>
2012-06-19 11:57:03 +0530

    added macro for toggle read mode in eamcs and modified installation script

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cp emacs ~/.emacs.tmp
-cp -r emacs.d ~/.emacs.d.tmp
-cp gitconfig ~/.gitconfig.tmp
-mkdir ~/xmonad
-cp xmonad.hs ~/xmonad/xmonad.hs
-cp xmobarrc ~/.xmobarrc.tmp
+cp emacs ~/.emacs
+cp -r emacs.d ~/.emacs.d
+cp gitconfig ~/.gitconfig
+mkdir ~/.xmonad
+cp xmonad.hs ~/.xmonad/xmonad.hs
+cp xmobarrc ~/.xmobarrc
