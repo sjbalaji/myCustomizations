fe00f2a8b9a86e31ff6eed02a8bd0b04b850bf22
balaji.sj <sjbalajimdu@gmail.com>
2012-06-20 22:41:08 +0530

    full comments

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 echo Configuring emcas
 cp emacs ~/.emacs
+cp -r emacs.d ~/.emacs.d
 echo Configuring screen
 cp screenrc ~/.screenrc
-cp -r emacs.d ~/.emacs.d
+echo Configuring git
 cp gitconfig ~/.gitconfig
+echo Configuring xmonad
 mkdir ~/.xmonad
 cp xmonad.hs ~/.xmonad/xmonad.hs
+echo Cinfiguring xmobar
 cp xmobarrc ~/.xmobarrc
