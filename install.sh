7c0ab12923a4af419a41061750aef366f8545f1a
balaji.sj <sjbalajimdu@gmail.com>
2012-07-02 21:05:44 +0530

    Mon Jul  2 21:05:43 IST 2012

--- install.sh
+++ install.sh
@@ -4,15 +4,15 @@ cp screenrc ~/.screenrc
 echo Configuring git
 cp gitconfig ~/.gitconfig
 echo Configuring xmonad
+rm -rf ~/.xmonad
 mkdir ~/.xmonad
 cp xmonad.hs ~/.xmonad/xmonad.hs
 echo Configuring xmobar
 cp -r  xmobarrc ~/.xmobarrc
 echo Configuring emcas
+rm -rf ~/.emacs*
 cp emacs ~/.emacs
 cp -r emacs.d ~/.emacs.d
 echo compiling emms 
 cd ~/.emacs.d/emms-3.0/;
 make;
-# echo installing emms
-# sudo make install;
