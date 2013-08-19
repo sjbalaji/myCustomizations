ac94e9d6c0ca4df62e35157c57029f53cea9c2d4
balaji.sj <sjbalajimdu@gmail.com>
2013-08-19 22:46:27 +0530

    Working xmonad config

---
+++ xmonad/wireless.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+iwconfig wlan2 2>&1 | grep -q no\ wireless\ extensions\. && {
+  echo wired
+  exit 0
+}
+
+essid=`iwconfig wlan2 | awk -F '"' '/ESSID/ {print $2}'`
+stngth=`iwconfig wlan2 | awk -F '=' '/Quality/ {print $2}' | cut -d '/' -f 1`
+#echo $stngth
+bars=`expr $stngth / 7`
+case $bars in
+  0)  bar="[----------]" ;;
+  1)  bar="[*---------]" ;;
+  2)  bar="[**--------]" ;;
+  3)  bar="[***-------]" ;;
+  4)  bar="[****------]" ;;
+  5)  bar="[*****-----]" ;;
+  6)  bar="[******----]" ;;
+  7)  bar="[*******---]" ;;
+  8)  bar="[********--]" ;;
+  9)  bar="[*********-]" ;;
+  10) bar="[**********]" ;;
+  *)  bar="[----!!----]" ;;
+esac
+echo -ne "${essid}: ${bar}"
+exit 0
